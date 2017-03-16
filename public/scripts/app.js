ee335972dbe744787452597253d70ab3484fd49f
emamiali <emamiali1993@gmail.com>
2017-03-16 15:08:11 -0700

    made the update function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         success: renderAllFood
     });
 
-    $('.submit').on('click',function(e) {
+    $('.submit').on('click', function(e) {
         e.preventDefault();
         console.log(e);
         console.log('button is clicked');
@@ -39,7 +39,7 @@ $(document).ready(function() {
     $('.foodList').on('click', '.delete-button', handleDeleteFoodClick);
 
     function handleDeleteFoodClick(e) {
-      console.log('delete-button was clicked');
+        console.log('delete-button was clicked');
         var foodId = $(this).closest('.food').data('food-id');
         console.log('this is data', foodId);
         console.log('this food is being deleted= ' + foodId);
@@ -63,6 +63,69 @@ $(document).ready(function() {
 
 
 
+
+    //===============================
+    $('.foodList').on('click', '.edit-food', handleFoodEditClick);
+    $('.foodList').on('click', '.save-food', handleSaveChangesClick);
+
+    // when the edit button for an food is clicked
+    function handleFoodEditClick(e) {
+        var $foodRow = $(this).closest('.food');
+        var foodId = $foodRow.data('food-id');
+        console.log('edit food', foodId);
+        console.log($foodRow);
+
+        // show the save changes button
+        $foodRow.find('.save-food').toggleClass('hidden');
+        // hide the edit button
+        $foodRow.find('.edit-food').toggleClass('hidden');
+
+
+    // get the food name and replace its field with an input element
+    var foodName = $foodRow.find('span.food-name').text();
+    $foodRow.find('span.food-name').html('<input class="edit-food-name" value="' + foodName + '"></input>');
+
+    // get the calories and replace its field with an input element
+    var calories = $foodRow.find('span.calories').text();
+    $foodRow.find('span.calories').html('<input class="edit-calories" value="' + calories + '"></input>');
+
+}
+    // after editing an food, when the save changes button is clicked
+    function handleSaveChangesClick(e) {
+        var foodId = $(this).parents('.food').data('food-id'); // $(this).closest would have worked fine too
+        var $foodRow = $('[data-food-id=' + foodId + ']');
+
+        var data = {
+            foodName: $foodRow.find('.edit-food-name').val(),
+            calories: $foodRow.find('.edit-calories').val(),
+        };
+        console.log('PUTing data for food', foodId, 'with data', data);
+        console.log(data);
+        $.ajax({
+            method: 'PUT',
+            url: '/api/food/' + foodId,
+            data: data,
+            success: handleFoodUpdatedResponse
+        });
+
+
+    function handleFoodUpdatedResponse(potato) {
+      console.log(potato);
+        console.log('response to update', potato);
+
+        var foodId = potato._id;
+        console.log(foodId);
+        // scratch this food from the page
+        $('[data-food-id=' + foodId + ']').remove();
+        // and then re-draw it with the updates ;-)
+        renderFood(data);
+    }
+  }
+    //===============================
+
+
+
+
     function renderFood(food) {
 
         var foodHTML = (`
@@ -71,7 +134,7 @@ $(document).ready(function() {
       <div class="col-md-10 col-md-offset-1" >
         <div class="panel panel-default">
           <div class="panel-body">
-            <div class='row'>
+            <div class='row meal'>
               <div class="col-md-9 col-xs-12">
                 <ul class="list-group">
                   <li class="list-group-item">
@@ -86,9 +149,9 @@ $(document).ready(function() {
               </div>
             </div>
             <div class='panel-footer'>
-            <div id='delete-button'>
             <button class='btn btn-danger delete-button'>Delete Food</button>
-            </div>
+            <button class='btn btn-info edit-food'>Edit Food</button>
+                <button class='btn btn-success save-food hidden'>Save Changes</button>
             </div>
           </div>
         </div>
