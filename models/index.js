fc3bcbf20f66ab50ccf3a05d196289fe44acd1d8
emamiali <emamiali1993@gmail.com>
2017-03-16 18:35:07 -0700

    pusing to heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/project1');
-
+mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 var Food = require('./food');
 
 module.exports.Food = Food;
